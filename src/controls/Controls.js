ba7c09a5a7abd1a64b7781ff926fec1eb94dcb67
john2796 <jbmiranda22796@gmail.com>
2019-03-06 11:47:29 -0800

    Project Set Up completed

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-const Controls = props => {
-  const { locked, closed, toggleLocked, toggleClosed } = props;
-
-  return (
-    <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
-        {locked ? 'Unlock Gate' : 'Lock Gate'}
-      </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
-        {closed ? 'Open Gate' : 'Close Gate'}
-      </button>
-    </div>
-  );
-};
+const Controls = (locked, closed, toggleLocked, toggleClosed) => (
+  <div className="controls panel">
+    <button type="button" disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+      {locked ? 'Unlock Gate' : 'Lock Gate'}
+    </button>
+    <button type="button" disabled={locked} onClick={toggleClosed} className="toggle-btn">
+      {closed ? 'Open Gate' : 'Close Gate'}
+    </button>
+  </div>
+);
 
 export default Controls;
