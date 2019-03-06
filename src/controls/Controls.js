eafeb8d0f59733fe97aac5099c5056cf4e7fff65
john2796 <jbmiranda22796@gmail.com>
2019-03-06 13:24:07 -0800

    gate && Display Component Completed

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Controls = (locked, closed, toggleLocked, toggleClosed) => (
+const Controls = ({locked, closed, toggleLocked, toggleClosed,}) => (
   <div className="controls panel">
-    <button type="button" disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+    <button 
+    data-testid="closegate"
+    type="button" disabled={!closed} onClick={toggleLocked} className="toggle-btn">
       {locked ? 'Unlock Gate' : 'Lock Gate'}
     </button>
-    <button type="button" disabled={locked} onClick={toggleClosed} className="toggle-btn">
+
+
+
+    <button 
+    data-testid="locked"
+    type="button" disabled={locked} onClick={toggleClosed} className="toggle-btn">
       {closed ? 'Open Gate' : 'Close Gate'}
     </button>
   </div>
 );
 
+Controls.propTypes = {
+  locked: PropTypes.bool,
+  closed: PropTypes.bool,
+  toggleLocked: PropTypes.func,
+  toggleClosed: PropTypes.func,
+};
 export default Controls;
--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,14 +1,21 @@
-import { render, fireEvent } from 'react-testing-library';
-import 'jest-dom/extend-expect';
-import renderer from 'react-testing-library';
-
-
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';import renderer from 'react-testing-library';
 import Controls from './Controls';// Test away!
+
+
+afterEach(cleanup);
 // ### Gate
 // -[x] defaults to `unlocked` and `open`
 // -[x] cannot be closed or opened if it is locked
 
 describe('<Control />', () => {
+  it('defaults to `unlocked` and `open`', () => {
+    const {getByTestId} = render(<Controls />)
+    
+    const closed = getByTestId("closegate")
+    const locked = getByTestId("locked")
 
-})
-;
+    expect(closed.disabled).toBeTruthy();
+    expect(locked.disabled).toBeFalsy()
+  });
+});
