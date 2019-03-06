ac126070f02ee6a5422f5f07d16f1f38bb7e845e
john2796 <jbmiranda22796@gmail.com>
2019-03-06 11:57:25 -0800

    Gate adding ... ☠️

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import renderer from 'react-testing-library';
+
+
+import Controls from './Controls';// Test away!
+// ### Gate
+// -[x] defaults to `unlocked` and `open`
+// -[x] cannot be closed or opened if it is locked
+
+describe('<Control />', () => {
+
+})
+;
